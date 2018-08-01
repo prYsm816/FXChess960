4f9941dc98c013e0510075f0f83f2cd00985e66c
Dan Christensen <816.dan@gmail.com>
2018-08-01 16:16:05 -0600

    Updated formatting and various errors.

--- src/javachessgui/Gui.java
+++ src/javachessgui/Gui.java
@@ -6,37 +6,33 @@ import javafx.scene.layout.HBox;
 
 
 public class Gui {
-    
+
+    public HBox horizontal_box = new HBox(2);
     private Board b;
     private Game g;
     private Stage s;
-    
-    public HBox horizontal_box=new HBox(2);
-    
-    public void shutdown()
-    {
-        b.stop_engine_process();
-    }
-    
-    public Gui(Stage set_s)
-    {
-              
-        s=set_s;
-        
-        b=new Board(true);
-        b.s=s;
-        
-        g=new Game(s,b);
-        
+
+    public Gui(Stage set_s) {
+
+        s = set_s;
+
+        b = new Board(true);
+        b.s = s;
+
+        g = new Game(s, b);
+
         g.reset(b.report_fen());
-        
-        b.g=g;
-        
-        
-        
+
+        b.g = g;
+
+
         horizontal_box.getChildren().add(b.main_box);
         horizontal_box.getChildren().add(g.vertical_box);
-        
+
     }
-    
+
+    public void shutdown() {
+        b.stop_engine_process();
+    }
+
 }
